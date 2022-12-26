b331fd8eb5cbca6756e49656b25b566522e48b91
a-rudolph <adam.rudolph@live.ca>
2022-12-26 01:05:02 -0500

    allow cors

--- api/v1/profile.js
+++ api/v1/profile.js
@@ -1,4 +1,24 @@
-export default async function (req, res) {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+const handler = async (req, res) => {
   const response = await fetch("/public/profiles.json");
   const profiles = await response.json();
 
@@ -12,4 +32,6 @@ export default async function (req, res) {
   }
 
   res.status(200).json({ data });
-}
+};
+
+export default allowCors(handler);
