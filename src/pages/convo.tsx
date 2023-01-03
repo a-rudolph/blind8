bf0cbbc4b235c14a3ea3dc1a15c12fc8aa2b2a35
a-rudolph <adam.rudolph@live.ca>
2023-01-03 19:51:40 +0000

    chat header and input

--- src/pages/convo.tsx
+++ src/pages/convo.tsx
@@ -4,18 +4,29 @@ import { useMemo } from "react";
 
 const ConvoPage = () => {
   return (
-    <div className="flex flex-col gap-6">
-      {Array(8)
-        .fill(1)
-        .map((_message, i) => {
-          return (
-            <Message
-              dir={i % 2 === 0 ? "left" : "right"}
-              content={lorem.generateSentences(1)}
-              key={i}
-            />
-          );
-        })}
+    <div className="flex flex-col h-page">
+      <div className="w-extra bg-white font-bold text-lg shadow-md mb-4 py-2">
+        Chatter's name
+      </div>
+      <div className="flex flex-col gap-6 flex-grow">
+        {Array(3)
+          .fill(1)
+          .map((_message, i) => {
+            return (
+              <Message
+                dir={i % 2 === 0 ? "left" : "right"}
+                content={lorem.generateSentences(1)}
+                key={i}
+              />
+            );
+          })}
+      </div>
+      <div className="w-extra bg-white flex mt-4 border-2 border-b-mono-3">
+        <input className="p-4 flex-grow" placeholder="hi there! 👋" />
+        <button className="font-bold rounded-sm text-xl text-white bg-primary-1 mr-[-16px] p-4 px-6">
+          {">"}
+        </button>
+      </div>
     </div>
   );
 };
